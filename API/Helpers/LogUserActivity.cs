53f82c0f841d0f27e6151e503568782e1ce78784
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-01-06 02:04:06 +0600

    make the last action filter more optimal

--- API/Helpers/LogUserActivity.cs
+++ API/Helpers/LogUserActivity.cs
@@ -15,10 +15,10 @@ namespace API.Helpers
 
             if(!resutContext.HttpContext.User.Identity.IsAuthenticated) return;
 
-            var username = resutContext.HttpContext.User.GetUsername();
+            var userId = resutContext.HttpContext.User.GetUserId();
             var repo = resutContext.HttpContext.RequestServices.GetService<IUserRepository>();
 
-            var user = await repo.GetUserByUsernameAsync(username);
+            var user = await repo.GetUserByIdAsync(userId);
             user.LastActive = DateTime.Now;
             await repo.SaveAllAsync();
         }
